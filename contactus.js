d27bde7795c99f75431a27319e2d68ab8bb7e892
Elena Barlow <elena.barlow1998@gmail.com>
2022-11-13 00:59:15 +0000

    deleted uneeded files

---
+++ contactus.js
@@ -0,0 +1,13 @@
+$('.signup').hide();
+
+    $('#signin, #signup').on(
+      'click',
+      function(){
+        $('.signin, .signup').toggle()
+        $('.form-input').removeClass()
+      }
+    )  
+
+document.querySelectorAll("form-input"), function close() {
+   
+}
